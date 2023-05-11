b07db68b83e825feab924bdf37941c366a40b9b5
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-05-11 15:02:48 +0600

    login registration api created

--- salesproject/taskone/tests.py
+++ salesproject/taskone/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
 
 # Create your tests here.
+class RegistrationLoginAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.register_url = reverse('user-register')
+        self.login_url = reverse('user-login')
+
+        self.user_data = {
+            'email': 'test@example.com',
+            'password': 'testpassword',
+            'first_name': 'abu',
+            'last_name': 'rakib'
+        }
+
+    def test_user_registration(self):
+        response = self.client.post(self.register_url, self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['email'], self.user_data['email'])
+
+    def test_user_login(self):
+        # Register the user first
+        self.client.post(self.register_url, self.user_data)
+
+        # Try to login with correct credentials
+        login_data = {
+            'email': self.user_data['email'],
+            'password': self.user_data['password']
+        }
+        response = self.client.post(self.login_url, login_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+        # Try to login with incorrect credentials
+        incorrect_login_data = {
+            'email': self.user_data['email'],
+            'password': 'incorrectpassword'
+        }
+        response = self.client.post(self.login_url, incorrect_login_data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- salesproject/taskone/urls.py
+++ salesproject/taskone/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import RegistrationAPIView, LoginAPIView
 
 urlpatterns = [
-    path('register/', RegistrationAPIView, name="user-register"),
+    path('register/', RegistrationAPIView.as_view(), name="user-register"),
     path('login/', LoginAPIView.as_view(), name='user-login'),
 ]
