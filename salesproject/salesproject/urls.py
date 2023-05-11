702284f9501096768ef8e326cd5fa84b03e14173
ABS-RAKIB <abu35-1994@diu.edu.bd>
2023-05-11 15:11:06 +0600

    login registration api created

--- salesproject/salesproject/urls.py
+++ salesproject/salesproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('taskone.urls')),
+    path('api/v2/', include('tasktwo.urls')),
 ]
